93b0f3bc68a927d242da5f7082b892ce8552a067
Wordz278 <floshikwambana@gmail.com>
2026-07-06 03:42:25 +0200

    initial commit

---
+++ src/main/java/com/adrian/customer/CustomerService.java
@@ -0,0 +1,48 @@
+package com.adrian.customer;
+
+import com.adrian.exception.DuplicateResourceException;
+import com.adrian.exception.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomerService {
+    private final CustomerDao customerDao;
+
+    public CustomerService(@Qualifier("jpa") CustomerDao customerDao) {
+        this.customerDao = customerDao;
+    }
+
+    public List<Customer> getAllCustomers(){
+        return customerDao.selectAllCustomers();
+    }
+
+    public Customer getCustomer(Integer id){
+        return customerDao.selectCustomerById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "Customer with id [%s] not found".formatted(id)
+                ));
+    }
+
+    public void addCustomer(
+            CustomerRegistrationRequest customerRegistrationRequest){
+        //check if email exists
+        String email = customerRegistrationRequest.email();
+        if(customerDao.existsPersonWithEmail(email)){
+            throw new DuplicateResourceException(
+                    "email already taken"
+            );
+        }
+
+        //add
+        Customer customer = new Customer(
+                customerRegistrationRequest.name(),
+                customerRegistrationRequest.email(),
+                customerRegistrationRequest.age()
+        );
+        customerDao.insertCustomer(customer);
+
+    }
+}
